eecba7eba0b18be0e202744d137dbdc87a31fa70
Harry Stew <harrystew@gmail.com>
2021-12-02 19:11:24 +0000

    add day02 solutions

---
+++ day02.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<string> split(const string& s, const string& delimiter) {
+    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+    string token;
+    vector<string> res;
+
+    while ((pos_end = s.find (delimiter, pos_start)) != string::npos) {
+        token = s.substr (pos_start, pos_end - pos_start);
+        pos_start = pos_end + delim_len;
+        res.push_back(token);
+    }
+
+    res.push_back(s.substr(pos_start));
+    return res;
+}
+
+int day02a(const vector<string>& commands) {
+    int x = 0, y = 0;
+    for (const string& cmd : commands) {
+        auto split_cmd = split(cmd, " ");
+        string direction = split_cmd.at(0);
+        int distance = stoi(split_cmd.at(1));
+        if (direction == "forward") x += distance;
+        else if (direction == "up") y -= distance;
+        else if (direction == "down") y += distance;
+        else throw std::invalid_argument("unknown direction: " + direction);
+    }
+    return x * y;
+}
+
+int day02b(const vector<string>& commands) {
+    int x = 0, y = 0, aim = 0;
+    for (const string& cmd : commands) {
+        auto split_cmd = split(cmd, " ");
+        string direction = split_cmd.at(0);
+        int val = stoi(split_cmd.at(1));
+        if (direction == "forward") {
+            x += val;
+            y += aim * val;
+        }
+        else if (direction == "up") aim -= val;
+        else if (direction == "down") aim += val;
+        else throw std::invalid_argument("unknown direction: " + direction);
+    }
+    return x * y;
+}
--- test.cpp
+++ test.cpp
@@ -1,10 +1,17 @@
 #include <gtest/gtest.h>
 #include "day01.cpp"
+#include "day02.cpp"
 
 using namespace std;
 
 TEST(Samples, Day01) {
-    vector<int> sample_input = {199, 200, 208, 210, 200, 207, 240, 269, 260, 263};
+    vector<int> sample_input = { 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 };
     EXPECT_EQ(day01a(sample_input), 7);
     EXPECT_EQ(day01b(sample_input), 5);
+}
+
+TEST(Samples, Day02) {
+    vector<string> sample_input = { "forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2" };
+    EXPECT_EQ(day02a(sample_input), 150);
+    EXPECT_EQ(day02b(sample_input), 900);
 }
