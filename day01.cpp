ea5bef11448eee20062a78b926990d7ca384bf44
Harry Stew <harrystew@gmail.com>
2021-12-02 19:23:25 +0000

    refactor day01 for consistency

--- day01.cpp
+++ day01.cpp
@@ -3,9 +3,10 @@
 
 using namespace std;
 
-int day01a(vector<int> &depths) {
+int day01a(const vector<string>& depths) {
     int prev_depth = INT32_MAX, depth_increases = 0;
-    for (int depth : depths) {
+    for (const string& depth_text : depths) {
+        int depth = stoi(depth_text);
         if (depth > prev_depth) {
             depth_increases++;
         }
@@ -14,7 +15,11 @@ int day01a(vector<int> &depths) {
     return depth_increases;
 }
 
-int day01b(vector<int> &depths) {
+int day01b(const vector<string>& lines) {
+    vector<int> depths;
+    depths.resize(lines.size() ); // unfortunately this is necessary
+    transform(lines.begin(), lines.end(), depths.begin(), [](const string& s) { return stoi(s); });
+
     int prev_window = INT32_MAX, depth_increases = 0;
     for (int i = 0; i < depths.size() - 2; i++) {
         int current_window = depths.at(i) + depths.at(i+1) + depths.at(i+2);
--- test.cpp
+++ test.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 TEST(Samples, Day01) {
-    vector<int> sample_input = { 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 };
+    vector<string> sample_input = { "199", "200", "208", "210", "200", "207", "240", "269", "260", "263" };
     EXPECT_EQ(day01a(sample_input), 7);
     EXPECT_EQ(day01b(sample_input), 5);
 }
