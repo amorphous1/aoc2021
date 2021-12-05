63f11294506c89c579ba3bd6bd8d847d6f98b3e0
Harry Stew <harrystew@gmail.com>
2021-12-05 17:54:28 +0000

    remove debug output

--- test.cpp
+++ test.cpp
@@ -25,7 +25,6 @@ TEST(Samples, Day03) {
 }
 
 TEST(Samples, Day04) {
-    cout << "test started";
     vector<string> sample_input = {
             "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1",
             "",
