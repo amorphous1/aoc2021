61953f87fc74b52a8840e8d2e268749857a0f6dc
Harry Stew <harrystew@gmail.com>
2021-12-16 14:12:47 +0000

    fix day 15 part 2 solution
    this is still quite slow, could use some optimisation...

--- day15.cpp
+++ day15.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <map>
 #include <set>
+#include <climits>
 
 using namespace std;
 
@@ -21,30 +22,41 @@ map<pair<int, int>, unsigned long> parse_risk_levels(const vector<string>& lines
     return risk;
 }
 
-unsigned long min_risk(const map<pair<int, int>, unsigned long> &risk, const int max_idx) {
+unsigned long total_risk(const pair<int, int>& pos, const map<pair<int, int>, unsigned long>& risk, map<pair<int, int>, unsigned long>& min_total_risk ) {
+    return (risk.contains(pos) && min_total_risk.contains(pos)) ? risk.at(pos) + min_total_risk.at(pos) : ULONG_MAX;
+}
+
+unsigned long find_min_risk(const map<pair<int, int>, unsigned long>& risk, int max_idx) {
     map<pair<int, int>, unsigned long> min_total_risk;
-    for (int y = max_idx; y >= 0; y--) {
-        for (int x = max_idx; x >= 0; x--) {
-            if (x == max_idx && y == max_idx) {
-                min_total_risk[make_pair(x, y)] = risk.at(make_pair(x, y));
-            } else if (x == max_idx) {
-                min_total_risk[make_pair(x, y)] = risk.at(make_pair(x, y)) + min_total_risk[make_pair(x, y + 1)];
-            } else if (y == max_idx) {
-                min_total_risk[make_pair(x, y)] = risk.at(make_pair(x, y)) + min_total_risk[make_pair(x + 1, y)];
-            } else {
-                min_total_risk[make_pair(x, y)] = risk.at(make_pair(x, y)) + min(min_total_risk[make_pair(x + 1, y)], min_total_risk[make_pair(x, y + 1)]);
+    bool min_total_risk_changed;
+    do {
+        min_total_risk_changed = false;
+        for (int y = max_idx; y >= 0; y--) {
+            for (int x = max_idx; x >= 0; x--) {
+                pair<int, int> pos = make_pair(x, y);
+                unsigned long old_min_total_risk = min_total_risk.contains(pos) ? min_total_risk.at(pos) : ULONG_MAX;
+                unsigned long new_min_total_risk = (x == max_idx && y == max_idx) ? 0 : min(
+                        min(total_risk(make_pair(x - 1, y), risk, min_total_risk),
+                            total_risk(make_pair(x + 1, y), risk, min_total_risk)),
+                        min(total_risk(make_pair(x, y - 1), risk, min_total_risk),
+                            total_risk(make_pair(x, y + 1), risk, min_total_risk))
+                    );
+                if (new_min_total_risk < old_min_total_risk) {
+                    min_total_risk_changed = true;
+                    min_total_risk[pos] = new_min_total_risk;
+                }
             }
         }
-    }
-    return min_total_risk.at(make_pair(0, 0)) - risk.at(make_pair(0, 0));
+    } while (min_total_risk_changed);
+    return min_total_risk.at(make_pair(0, 0));
 }
 
 unsigned long day15a(const vector<string>& lines) {
     map<pair<int, int>, unsigned long> risk = parse_risk_levels(lines, 0);
-    return min_risk(risk, (int)lines.size() - 1);
+    return find_min_risk(risk, (int)lines.size() - 1);
 }
 
 unsigned long day15b(const vector<string>& lines) {
     map<pair<int, int>, unsigned long> risk = parse_risk_levels(lines, 4);
-    return min_risk(risk, (int)(5 * lines.size() - 1));
+    return find_min_risk(risk, (int)(5 * lines.size() - 1));
 }
