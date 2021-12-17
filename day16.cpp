2b5c7fd39ae561486b1e22909455b3c0e3dc602e
Harry Stew <harrystew@gmail.com>
2021-12-17 22:06:11 +0000

    remove debug function, no need to store packet length

--- day16.cpp
+++ day16.cpp
@@ -10,7 +10,6 @@ struct Packet {
     string type;
     vector<Packet> children;
     unsigned long value;
-    int length;
 
     int version_sum() {
         int result = version;
@@ -19,45 +18,33 @@ struct Packet {
         }
         return result;
     }
-    string repr() {
-        string result = " { " + type + " = " + to_string(value) + " #" + to_string(length) + " [";
-        for (Packet child : children) {
-            result += child.repr();
-        }
-        result += "] } ";
-        return result;
-    }
 };
 
-Packet parse_packet(const string& packet, int start) {
+Packet parse_packet(const string& packet, int& pos) {
     const map<int, string> bits_operators = { {0, "sum"}, {1, "product"}, {2, "min"}, {3, "max"}, {4, "val"}, {5, "gt"}, {6, "lt"}, {7, "eq"}, };
-    const int version = stoi(packet.substr(start, 3), nullptr, 2);
-    const string type = bits_operators.at(stoi(packet.substr(start + 3, 3), nullptr, 2));
-    int pos = start + 6;
+    const int version = stoi(packet.substr(pos, 3), nullptr, 2);
+    const string type = bits_operators.at(stoi(packet.substr(pos + 3, 3), nullptr, 2));
+    pos += 6;
     if (type == "val") { // literal value
         string value;
         do {
             value += packet.substr(pos + 1, 4);
             pos += 5;
         } while (packet.at(pos - 5) == '1');
-        return { version, type, {}, stoul(value, nullptr, 2), pos - start };
+        return { version, type, {}, stoul(value, nullptr, 2) };
     }
     vector<Packet> children;
     if (packet.at(pos) == '0') { // 15 bit representing total size of children in bits
         const int end_pos = pos + 1 + 15 + stoi(packet.substr(pos + 1, 15), nullptr, 2);
         pos += 1 + 15;
         while (pos < end_pos) {
-            Packet child = parse_packet(packet, pos);
-            pos += child.length;
-            children.push_back(child);
+            children.push_back(parse_packet(packet, pos));
         }
     } else { // 11 bit representing number of children
         const int num_children = stoi(packet.substr(pos + 1, 11), nullptr, 2);
         pos += 1 + 11;
         for (int i = 1; i <= num_children; i++) {
-            const Packet child = parse_packet(packet, pos);
-            pos += child.length;
-            children.push_back(child);
+            children.push_back(parse_packet(packet, pos));
         }
     }
     unsigned long value = 0;
@@ -88,7 +75,7 @@ Packet parse_packet(const string& packet, int start) {
     } else {
         throw invalid_argument("unknown operator type " + type);
     }
-    return { version, type, children, value, pos - start };
+    return { version, type, children, value };
 }
 
 string convert_to_binary(const string &hex_input) {
@@ -106,11 +93,13 @@ string convert_to_binary(const string &hex_input) {
 }
 
 unsigned long day16a(const string& hex_input) {
-    Packet packet = parse_packet(convert_to_binary(hex_input), 0);
+    int start_pos = 0;
+    Packet packet = parse_packet(convert_to_binary(hex_input), start_pos);
     return packet.version_sum();
 }
 
 unsigned long day16b(const string& hex_input) {
-    Packet packet = parse_packet(convert_to_binary(hex_input), 0);
+    int start_pos = 0;
+    Packet packet = parse_packet(convert_to_binary(hex_input), start_pos);
     return packet.value;
 }
